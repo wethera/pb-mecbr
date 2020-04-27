9f6c15efa570d48ad979997b977fe29d12218d54
wethera <wethera@outlook.com>
2020-04-27 14:53:38 -0500

    Commit Mon Apr 27 14:53:37 CDT 2020

--- specs/gpu/cluster-init/scripts/01_gpu_conf.sh
+++ specs/gpu/cluster-init/scripts/01_gpu_conf.sh
@@ -2,5 +2,12 @@
 set -ex
 #Create link to gres.conf
 ln -s /sched/gres.conf /etc/slurm/gres.conf
+echo "Linked gres.conf file"
+
 #Disable power mgmt
 /usr/bin/nvidia-smi -pm 1
+echo "NVIDIA power modulation disabled"
+
+#Load nvidia_uvm module
+modprobe nvidia_uvm
+echo "NVIDIA UNified Memory Module loaded"
